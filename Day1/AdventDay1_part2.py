ae3f630f458037f65d8ae557f38d272a96954e3f
Laëtitia Cabrol <laetitia.cabrol@hotmail.fr>
2025-11-08 14:21:37 +0100

    Initial commit - Day 1

---
+++ Day1/AdventDay1_part2.py
@@ -0,0 +1,9 @@
+if (__name__ == '__main__'):
+    # Open the input file "input.txt" in read mode
+    with open("input1.txt", "r") as f:
+        lines = f.readlines()  # Read all lines from the file into a list
+        left_column = sorted([int(line.split()[0]) for line in lines])
+        right_column = sorted([int(line.split()[1]) for line in lines])
+        final_sum = 0
+        for i in range(len(left_column)): final_sum += left_column[i] * right_column.count(left_column[i])
+        print(final_sum)
