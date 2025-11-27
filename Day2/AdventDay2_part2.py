203773114d3757cef60b7384bd27e1b9d6ea62f2
Laëtitia Cabrol <laetitia.cabrol@hotmail.fr>
2025-11-27 20:10:25 +0100

    Day 2 part 2 commented

--- Day2/AdventDay2_part2.py
+++ Day2/AdventDay2_part2.py
@@ -30,17 +30,27 @@ if (__name__ == '__main__'):
                 final_sum += 1
             
             else:
+                # Try removing one element to achieve safety
                 safe=True
+                # Check all possible removals
                 for remove_index in range(len(line)):
                     new_line = line[:remove_index] + line[remove_index+1:]
+                    # Determine the overall trend for the new line
                     increase_or_decrease = int(new_line[1]) - int(new_line[0])
+                    # Check safety for the new line
                     for j in range (0,len(new_line)-1):
+                        # Calculate the difference between consecutive elements
                         difference = int(new_line[j+1]) - int(new_line[j])
+                        # Check the safety conditions
                         safe = check_safety(increase_or_decrease, difference)
                         if not safe:
                             break
+                    
                     if safe:
                         final_sum += 1
                         break
+                    
+                    # Reset safe to True for the next removal attempt
+                    safe=True
             
         print(final_sum)
